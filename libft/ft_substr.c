c01bf1903dd863c8329c9a240b61207c6f0587a0
Boldizsar Szabo <szboldisz@gmail.com>
2023-09-18 14:30:37 +0000

    Correct substr (mostly)

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -12,14 +12,37 @@
 
 #include <stddef.h>
 #include <stdlib.h>
+#include "libft.h"
+
+int	handle_edge(char const *str, unsigned int start, size_t len)
+{
+	size_t	str_len;
+
+	str_len = ft_strlen(str);
+	if (str == NULL)
+		return (1);
+	if (start >= str_len)
+		return (1);
+	if (len > str_len - start)
+		len = str_len - start;
+	return (0);
+}
 
 char	*ft_substr(char const *str, unsigned int start, size_t len)
 {
-	int		i;
-	char	*temp;
+	int				i;
+	char			*temp;
 
 	i = 0;
-	temp = (char *)malloc(sizeof(char) * len + 1);
+	if (handle_edge(str, start, len))
+	{
+		temp = (char *)malloc(sizeof(char));
+		if (temp == NULL)
+			return (NULL);
+		temp[0] = '\0';
+		return (temp);
+	}
+	temp = (char *)malloc(sizeof(char) * (len + 1));
 	if (temp == NULL)
 		return (NULL);
 	while (i < (int)len && str[i])
@@ -35,7 +58,7 @@ char	*ft_substr(char const *str, unsigned int start, size_t len)
 int main(void)
 {
 	char *str = "hello world";
-	char *substr = ft_substr(str, 6, 3);
+	char *substr = ft_substr(str, 4, 3);
 	printf("str: %s\nsubstr: %s\n", str, substr);
 	free(substr);
 	return (0);
